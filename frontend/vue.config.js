3f9a9b335c64d1c5fca69e4c6fd0e0362887c17a
hschoi1104 <hschoi1104@gmail.com>
2020-08-05 14:48:53 +0900

    connect backend and frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    publicPath: '/',
+    outputDir: '../backend/public/',
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3000/',
+                changeOrigin: true,
+                pathRewrite: { "^/apis": "" }
+            },
+        },
+    },
+}
