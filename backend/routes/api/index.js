f98383adb9ecebedf0ed825672f2cd41a25c210e
hschoi1104 <hschoi1104@gmail.com>
2020-08-08 20:54:23 +0900

    add module jsonwebtoken

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
-const user = require('./user')
-const cytoscape_info = require('./cytoscape_info')
-const article = require('./article')
+//const user = require('./user')
+//const cytoscape_info = require('./cytoscape_info')
+//const article = require('./article')
 
-router.use('/user',user)
-router.use('/cytoscape_info',cytoscape_info)
-router.use('/article',article)
+//router.use('/user',user)
+//router.use('/cytoscape_info',cytoscape_info)
+//router.use('/article',article)
 
 module.exports = router
