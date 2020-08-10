c1efe072bfc93d730c5ad2304c088eeef6d6d411
hschoi1104 <hschoi1104@gmail.com>
2020-08-10 13:02:45 +0900

    add cytoscape_info apis

---
+++ backend/routes/api/cytoscape_info/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const controller = require('./cytoscape_info.controller')
+
+router.put('/:user_id/:cyjson',controller.update)
+router.post('/',controller.write)
+router.get('/:user_id',controller.read)
+
+module.exports = router
