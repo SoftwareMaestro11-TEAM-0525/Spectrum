b2b312b9453372736d2fa43fdfbc57d0ad788b20
hschoi1104 <hschoi1104@gmail.com>
2020-08-20 18:46:39 +0900

    modify mongodbUri in config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongodbUri: "mongodb://mongo/spectrum",
+  mongodbUri: "mongodb://localhost:27017/spectrum",
   jwtKey: "sPeCtRuM-jWt-KeY",
   pwKey: "password-secreat-key",
 };
