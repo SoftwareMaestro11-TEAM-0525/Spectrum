dd730be198b3e64e9810498b5053f3ec645da450
hschoi1104 <hschoi1104@gmail.com>
2020-09-03 04:34:05 +0900

    Update index.route.js
    Add User.route.js

--- backend/routes/index.route.js
+++ backend/routes/index.route.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const auth = require("./auth.route");
 const cytoscape_info = require("./cytoscape_info.route");
-const article = require("./article.route");
+import article from "./article.route";
+import user from "./user.route";
 
 router.use("/auth", auth);
 router.use("/cytoscape_info", cytoscape_info);
 router.use("/article", article);
+router.use("/user",user);
 
 module.exports = router;
