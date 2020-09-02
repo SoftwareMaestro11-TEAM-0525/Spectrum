b67ae21a532ef1928e1ba752d8b15e53418c5fed
hschoi1104 <hschoi1104@gmail.com>
2020-09-03 04:31:40 +0900

    Update app.js
    Add Error handler

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,15 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
-import logger from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import indexRoute from "./routes/index.route.js";
-
+import logger from "morgan"
 var app = express();
 dotenv.config()
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -19,12 +18,18 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use("/api", indexRoute);
 
+// errorHandler
+app.use(function (err, req, res, next) {
+    logger(err.message)
+    return res.status(err.status || 500).json({success:false, status:err.status,message : err.message});
+});
+
 // Connect Mongodb
 const db = mongoose.connection;
 db.on("error", console.error);
 db.once("open", function () {
-  console.log("Connected to mongod server");
+  logger("Connected to mongod server");
 });
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
 
 module.exports = app;
