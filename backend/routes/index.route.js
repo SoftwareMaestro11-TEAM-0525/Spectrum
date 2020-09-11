ce7d56928e3362c2a32900631bf359302f54229a
hschoi1104 <hschoi1104@gmail.com>
2020-09-11 18:19:45 +0900

    Update index.route.js
    Add POST /api/file

--- backend/routes/index.route.js
+++ backend/routes/index.route.js
@@ -3,10 +3,12 @@ import article from "./article.route";
 import user from "./user.route";
 import auth from "./auth.route";
 import cytoscape from "./cytoscape.route";
+import file from "./file.route";
 
 router.use("/auth", auth);
 router.use("/cytoscape", cytoscape);
 router.use("/article", article);
 router.use("/user", user);
+router.use("/file", file);
 
 module.exports = router;
