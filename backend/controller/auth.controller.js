3723ea4ae68e507463f029f8f1852e04e04ed246
hschoi1104 <hschoi1104@gmail.com>
2020-09-21 03:24:33 +0900

    Update auth.controller.js
    Change Token info {user_id, user_email}

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -11,7 +11,12 @@ export class AuthController {
       const input_pw = User.user_pw;
       await AuthService.verifyUserPw({ user_pw, input_pw });
 
-      const token = await AuthService.createToken(req.body);
+      const userObj = {
+        user_id: User.user_id,
+        user_email: user_email,
+      };
+
+      const token = await AuthService.createToken(userObj);
       return res.status(200).json({
         success: true,
         message: "login success",
