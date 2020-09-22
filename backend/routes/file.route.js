8f9b8c63cafa18de1430342ee9cc1594947c0c45
hschoi1104 <hschoi1104@gmail.com>
2020-09-22 19:02:55 +0900

    Update file.route.js
    Add file upload Auth

--- backend/routes/file.route.js
+++ backend/routes/file.route.js
@@ -3,7 +3,6 @@ import { FileController } from "../controller/file.controller";
 import { upload } from "../util/upload";
 import authMiddleware from "../middlewares/auth";
 
-router.use("/check", authMiddleware);
-router.post("/", upload.single("file"), FileController.upload);
+router.post("/", authMiddleware, upload.single("file"), FileController.upload);
 
 module.exports = router;
