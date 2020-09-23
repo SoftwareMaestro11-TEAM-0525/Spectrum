4810d49995c1e22149dd7d8704cdc76ede2ea8ef
hschoi1104 <hschoi1104@gmail.com>
2020-09-24 02:01:49 +0900

    Update file.route.js
    Add delete /api/file/:user_id/:node_id/:key for delete s3 object api

--- backend/routes/file.route.js
+++ backend/routes/file.route.js
@@ -4,5 +4,9 @@ import { upload } from "../util/upload";
 import authMiddleware from "../middlewares/auth";
 
 router.post("/", authMiddleware, upload.single("file"), FileController.upload);
-
+router.delete(
+  "/:user_id/:node_id/:bucket/:key",
+  authMiddleware,
+  FileController.delete
+);
 module.exports = router;
