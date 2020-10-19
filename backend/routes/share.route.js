b5af6794d19a9aabbb822f959783135bde42c41e
hschoi1104 <hschoi1104@gmail.com>
2020-10-20 00:07:44 +0900

    Update share.route.js
    Add POST /api/share/mindmap

--- backend/routes/share.route.js
+++ backend/routes/share.route.js
@@ -4,10 +4,10 @@ import { ShareArticleController } from "../controller/share/article.controller";
 import authMiddleware from "../middlewares/auth";
 
 router.get("/mindmap/:share_key",ShareMindmapController.read);
-// router.post("/mindmap",authMiddleware,ShareMindmapController.create);
+router.post("/mindmap",authMiddleware,ShareMindmapController.create);
 //router.delete("/mindmap/:user_id",ShareMindmapController.delete);
 
 // router.get("/article/:share_key",ShareArticleController.read);
 // router.post("/article",authMiddleware,ShareArticleController.create);
-//router.delete("/article/:share_key",ShareArticleController.delete);
+// router.delete("/article/:share_key",ShareArticleController.delete);
 module.exports = router;
