cb7a974bb4b2182c8d613debc850be47921c9158
hschoi1104 <hschoi1104@gmail.com>
2020-10-20 15:22:30 +0900

    Update article.controller.js
    add create funtion in ShareArticleController

--- backend/controller/share/article.controller.js
+++ backend/controller/share/article.controller.js
@@ -1,6 +1,19 @@
 import { ShareArticleService } from "../../service/share/article.service";
 
 export class ShareArticleController {
+  static create = async (req, res, next) => {
+    try {
+      const result = await ShareArticleService.creatShareKey(req.body);
+      return res.status(201).json({
+        success: true,
+        message: "Create Share key Success",
+        result: result,
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
+
   static read = async (req, res, next) => {
     try {
       const result = await ShareArticleService.findByShareKey(
