12d030b6dc0aaac10207126cb5c54540f1939e8f
hschoi1104 <hschoi1104@gmail.com>
2020-10-20 15:34:25 +0900

    Update share.route.js
    Add DELETE /api/share/article/:sharae_key

--- backend/routes/share.route.js
+++ backend/routes/share.route.js
@@ -14,5 +14,5 @@ router.delete(
 
 router.get("/article/:share_key", ShareArticleController.read);
 router.post("/article", authMiddleware, ShareArticleController.create);
-// router.delete("/article/:share_key",ShareArticleController.delete);
+router.delete("/article/:share_key", ShareArticleController.delete);
 module.exports = router;
