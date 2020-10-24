c84f653b2046513aed97083ab6c9fd8ad4e885fc
hschoi1104 <hschoi1104@gmail.com>
2020-10-25 01:41:15 +0900

    Update article.route.js
    Fix error

--- backend/routes/article.route.js
+++ backend/routes/article.route.js
@@ -3,6 +3,11 @@ import { ArticleController } from "../controller/article.controller";
 import authMiddleware from "../middlewares/auth";
 
 router.get("/:user_id/:node_id", authMiddleware, ArticleController.read);
+router.get(
+  "/all/:user_id/:node_id",
+  authMiddleware,
+  ArticleController.readAllContent
+);
 router.post("/", authMiddleware, ArticleController.write);
 router.patch("/:user_id/:node_id", authMiddleware, ArticleController.update);
 router.delete("/:user_id/:node_id", authMiddleware, ArticleController.delete);
