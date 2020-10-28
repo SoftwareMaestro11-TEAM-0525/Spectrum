fffb37b4caae98bb23dd7473051aff8bf276e281
hschoi1104 <hschoi1104@gmail.com>
2020-10-28 16:10:46 +0900

    Update article.route.js
    read function Update

--- backend/routes/article.route.js
+++ backend/routes/article.route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 import { ArticleController } from "../controller/article.controller";
 import authMiddleware from "../middlewares/auth";
 
-router.get("/:user_id/:node_id", authMiddleware, ArticleController.read);
+router.get("/:user_id/:node_id", authMiddleware, ArticleController.readArticle);
 router.post("/", authMiddleware, ArticleController.write);
 router.patch("/:user_id/:node_id", authMiddleware, ArticleController.update);
 router.delete("/:user_id/:node_id", authMiddleware, ArticleController.delete);
