4fa72cfb979db8db2ad4b8b0ee86f921d7012656
hschoi1104 <hschoi1104@gmail.com>
2020-11-07 16:42:02 +0900

    Add POST /api/article/recommend

--- backend/routes/index.route.js
+++ backend/routes/index.route.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 import article from "./article.route";
-import articles from "./articles.route";
 import timeline from "./timeline.route";
 import user from "./user.route";
 import auth from "./auth.route";
@@ -11,7 +10,6 @@ import share from "./share.route";
 router.use("/auth", auth);
 router.use("/cytoscape", cytoscape);
 router.use("/article", article);
-router.use("/articles",articles);
 router.use("/timeline", timeline);
 router.use("/user", user);
 router.use("/file", file);
