498343c966282775de03e2a6181d54c459c1d851
hschoi1104 <hschoi1104@gmail.com>
2020-11-16 05:26:07 +0900

    Update article.route.js
    - 프론트엔드 토큰 관리 이해 불가로인한 API JWT Token 인증과정 생략

--- backend/routes/article.route.js
+++ backend/routes/article.route.js
@@ -11,7 +11,8 @@ router.get("/:user_id/:node_id", authMiddleware, ArticleController.readArticle);
 router.get("/:user_id", authMiddleware, ArticleController.readArticles);
 router.post("/", authMiddleware, ArticleController.write);
 router.patch("/:user_id/:node_id", authMiddleware, ArticleController.update);
-router.delete("/:user_id/:node_id", authMiddleware, ArticleController.delete);
+// router.delete("/:user_id/:node_id", authMiddleware, ArticleController.delete);
+router.delete("/:user_id/:node_id", ArticleController.delete);
 
 router.post(
   "/recommend",
