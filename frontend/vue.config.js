ab9db6b391bc9eef984198be199bbf3a82369bbd
naru200 <jinsu2504@naver.com>
2020-11-18 20:14:50 +0900

    edit backend path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://backend:3000",
         changeOrigin: true,
         pathRewrite: { "^/apis": "" }
       }
